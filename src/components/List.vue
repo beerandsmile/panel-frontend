67c883629ef8c35c1bf4a0b900b894acf8a6b24a
Pereverzev Daniil <prvrzv-daniil@mail.ru>
2019-03-05 00:52:31 +0300

    доделаны компоненты настроек

--- src/components/List.vue
+++ src/components/List.vue
@@ -7,23 +7,25 @@
         <v-expansion-panel-content v-for="(domain,i) in domains"
                                    :key="i">
           <template v-slot:header>
-            <div>Item</div>
+            <div>
+              <a :href="'http://' + domain.domain"
+                 class="headline">{{domain.domain}} </a><var class="blockquote">{{domain.work_dir}}</var>
+            </div>
           </template>
           <v-card>
-            <v-layout align-start
-                      justify-start
-                      row
-                      fill-height>
+            <v-divider />
+            <v-layout wrap
+                      row>
               <v-flex xs12
                       sm6
                       md4
-                      lg3 class="item">
+                      lg3>
                 <v-list dense>
-                  <template v-for="(port, i) in domain.ports">
-                    <v-divider :key="'port_list_divider' + i"></v-divider>
-                    <v-list-tile :key="'port' + i">
-                      <v-list-tile-content>{{port.port}}</v-list-tile-content>
-                      <v-list-tile-content class="align-end">{{port.prot_description}}</v-list-tile-content>
+                  <template v-for="(port, portName) in domain.port_range">
+                    <v-divider v-if="Object.keys(domain.port_range)[0] !== portName" :key="'port_list_divider' + portName"></v-divider>
+                    <v-list-tile :key="'port' + portName">
+                      <v-list-tile-content>{{portName}}</v-list-tile-content>
+                      <v-list-tile-content class="align-end">{{port}}</v-list-tile-content>
                     </v-list-tile>
                   </template>
                 </v-list>
@@ -31,8 +33,54 @@
               <v-flex xs12
                       sm6
                       md4
-                      lg3 class="item">
-                <v-switch v-model="domain.power" :label="`Домен ${domain.power ? 'включен' : 'выключен'}.`"></v-switch>
+                      lg3
+                      class="item">
+                <v-layout column
+                          wrap
+                          class="ml-4">
+                  <v-flex xs12
+                          sm6
+                          md4
+                          lg3
+                          class="item ">
+                    <v-switch v-model="domain.enabled"
+                              class="pt-0 mt-1 "
+                              @change="switchDomain($event, domain.prefix)"
+                              :label="`Домен ${domain.enabled ? 'включен' : 'выключен'}.`"></v-switch>
+                  </v-flex>
+                  <v-flex xs12
+                          sm6
+                          md4
+                          lg3
+                      class="pr-2">
+                    <v-textarea label="Конфиг"
+                                v-model="domain.package_json_config"></v-textarea>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+              <v-flex d-flex
+                      xs12
+                      sm6
+                      md4
+                      lg3
+                      offset-sm6
+                      offset-md3
+                      offset-lg3>
+                <v-layout align-end
+                          justify-end
+                          row
+                          fill-height>
+                  <v-btn flat
+                         icon
+                         color="red">
+                    <v-icon>delete</v-icon>
+                  </v-btn>
+                  <v-btn flat
+                         icon
+                         color="success">
+                    <v-icon>save</v-icon>
+                  </v-btn>
+                </v-layout>
               </v-flex>
             </v-layout>
           </v-card>
@@ -58,56 +106,52 @@ export default {
   data: () => ({
     domains: [
       {
-        ports: [
-          {
-            port: "1234",
-            prot_description: "text about app"
-          },
-          {
-            port: "1234",
-            prot_description: "text about app"
-          },
-          {
-            port: "1234",
-            prot_description: "text about app"
-          },
-          {
-            port: "1234",
-            prot_description: "text about app"
-          }
-        ],
-        power: true
-      },
-      {
-        ports: [
-          {
-            port: "1234",
-            prot_description: "text about app"
-          },
-          {
-            port: "1234",
-            prot_description: "text about app"
-          },
-          {
-            port: "1234",
-            prot_description: "text about app"
-          },
-          {
-            port: "1234",
-            prot_description: "text about app"
-          }
-        ],
-        power: true
+        prefix: "my",
+        domain: "my.login.buhojmedved.ru",
+        work_dir: "/home/login/www/my.login/",
+        enabled: true,
+        owner: "login",
+        package_json_config: {},
+        port_range: {
+          backend: 8081,
+          frontend: 8082,
+          dev: 8083
+        }
       }
     ]
-  })
+  }),
+  mounted() {
+    var that = this;
+    axios
+      .get("/my/domains")
+      .then(res => {
+        that.domains = res.data;
+      })
+      .catch(() => {});
+  },
+  methods: {
+    getPosrtName: function(aPorts, i) {
+      return Object.keys(aPorts)[i];
+    },
+    switchDomain: function(oEvent, prefix) {
+      var that = this;
+      axios
+        .patch("/my/domains", {
+          prefix: prefix,
+          switch: oEvent
+        })
+        .then(res => {
+          // that.domains = res.data;
+        })
+        .catch(() => {});
+    }
+  }
 };
 </script>
 
 <style>
-.item {
-  min-height: 50px;
-  min-width: 80px;
-  margin: 10px;
+.item,
+.item > div {
+  min-width: 180px;
 }
 </style>
